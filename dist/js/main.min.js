e67262f98e09a520e7eaf0a170026b5463e9f0e6
alistaircolling <alistair.colling@gmail.com>
2015-10-02 15:42:25 +0100

    added pages and nav - display is pushing up from the bottom for some reason. will investigate when styling rather than fix now. am going to lay out the rest of the app

--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -5,75 +5,71 @@ var routerApp = angular.module('routerApp', ['ui.router']);
 
 routerApp.config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/home');
-  // add states to the state provider
-  $stateProvider
+    $urlRouterProvider.otherwise('/home');
+    // add states to the state provider
+    $stateProvider
 
-  // HOME STATES AND NESTED VIEWS ========================================
-    .state('home', {
-    url: '/home',
-    templateUrl: 'partial-home.html',
-    controller: function($scope) {
-    }
-  })
-
-  //nested list with custom controller - I think the scope is passed and the dogs array is attached. presumably I could specify a controller with its own js file for example that requested the data from an API
-  .state('home.list', {
-      url: '/list',
-      templateUrl: 'partial-home-list.html',
-      controller: function($scope) {
-        $scope.dogs = ['Fenella', 'Monty', 'Coco'];
-      }
-    })
-    //nested paragraph with a string passed in template. again, presumably the template could specify
-    .state('home.paragraph', {
-      url: '/paragraph',
-      template: 'This was text passed from the router config.'
+    // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+        url: '/home',
+        templateUrl: 'partial-home.html',
+        controller: function($scope) {}
     })
 
-  //GALLERY ===========
-  .state('gallery', {
-    url: '/gallery',
-    templateUrl: 'partial-gallery.html',
-    controller: function($scope) {
-      // reset the scroll
-    }
-  })
+    //nested list with custom controller - I think the scope is passed and the dogs array is attached. presumably I could specify a controller with its own js file for example that requested the data from an API
+    .state('home.list', {
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
+            controller: function($scope) {
+                $scope.dogs = ['Fenella', 'Monty', 'Coco'];
+            }
+        })
+        //nested paragraph with a string passed in template. again, presumably the template could specify
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'This was text passed from the router config.'
+        })
+                   //BEDS ===========
+        .state('beds', {
+            url: '/beds',
+            views: {
+                '': {
+                    templateUrl: 'partial-beds.html'
+                },
+                'prices-intro@beds': {
+                    template: 'All rooms come with tea and coffee making facilities'
+                },
+                'prices@beds': {
+                    templateUrl: 'table-data.html',
+                    controller: 'pricesController'
+                }
+            }
+        })
 
-  //BOOK  ========
-
-  //GALLERY ===========
-  .state('book', {
-      url: '/book',
-      views: {
-        '': {
-          templateUrl: 'partial-book.html'
-        },
-        'prices-intro@book': {
-          template: 'All rooms come with tea and coffee making facilities'
-        },
-        'prices@book': {
-          templateUrl: 'table-data.html',
-          controller: 'pricesController'
+                    //LOCATION    ===========
+    .state('location', {
+        url: '/location',
+        templateUrl: 'partial-location.html',
+        controller: function($scope) {
+            // reset the scroll
         }
-      }
-  })
+    })
 
-// ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-.state('about', {
-  url: '/about',
-  views: {
-    //main template (notice blank string key)
-    '': {
-      templateUrl: 'partial-about.html',
-      controller: function($scope) {
-        // reset the scroll
-      }
-    }
+                  // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+    .state('about', {
+        url: '/about',
+        views: {
+            //main template (notice blank string key)
+            '': {
+                templateUrl: 'partial-about.html',
+                controller: function($scope) {
+                    // reset the scroll
+                }
+            }
 
-  }
+        }
 
-});
+    });
 
 });
 
@@ -84,16 +80,16 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 
 routerApp.controller('pricesController', function($scope) {
 
-  $scope.message = 'test';
+    $scope.message = 'test';
 
-  $scope.rooms = [{
-    name: 'Basic',
-    price: 50
-  }, {
-    name: 'En-suite',
-    price: 1000
-  }, {
-    name: 'Presidential',
-    price: 50000000
-  }]
+    $scope.rooms = [{
+        name: 'Basic',
+        price: 50
+    }, {
+        name: 'En-suite',
+        price: 1000
+    }, {
+        name: 'Presidential',
+        price: 50000000
+    }]
 })
