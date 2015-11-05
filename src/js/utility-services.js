a251e61611f4c90dc30a7d52811da621bb7600c7
alistaircolling <alistair.colling@gmail.com>
2015-11-05 14:32:49 +0000

    device service created to handle device checks

--- src/js/utility-services.js
+++
@@ -1,7 +0,0 @@
-//This service is a variable that stores if the user is using a mobile device 
-/*routerApp.factory('isMobile', ['$window',*/
-    //function(win) {
-        ////TODO not sure if this actually needs to be done...
-
-    //}
-/*]);*/
