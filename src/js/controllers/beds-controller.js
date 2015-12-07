067a598c6c730f90d84483de16cc45f0ba9bbc00
alistaircolling <alistair.colling@gmail.com>
2015-12-07 14:49:30 +0000

    ..

--- src/js/controllers/beds-controller.js
+++ src/js/controllers/beds-controller.js
@@ -1,10 +1,18 @@
 routerApp.controller('BedsController', function($scope) {
-    $scope.rooms = [
-    {
+    $scope.rooms = [{
         name: 'Basic',
         price: 50,
         copy: 'A great option if you are on a budget or travelling in a large group.',
-        image:"http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg"
-    }
-    ];
+        image: "http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg"
+    }, {
+        name: 'Deluxe',
+        price: 80,
+        copy: 'A great option if you are on a budget or travelling in a large group.',
+        image: "http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg"
+    }, {
+        name: 'Presidential Suite',
+        price: 200,
+        copy: 'A great option if you are on a budget or travelling in a large group.',
+        image: "http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg"
+    }];
 });
