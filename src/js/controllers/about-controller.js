43da3c257316e15f42ec17dab71466fdfc189730
alistaircolling <alistair.colling@gmail.com>
2015-12-07 12:40:11 +0000

    alternate r/l floats pulling data from scope

--- src/js/controllers/about-controller.js
+++ src/js/controllers/about-controller.js
@@ -1,6 +1,6 @@
 routerApp.controller('AboutController', function($scope) {
     //Could inject data service here
-    $scope.title = 'page-title';
+    $scope.title = 'Who stays here?';
 
     $scope.articles = [{
             copy: "'Come back!' the Caterpillar called after her. 'I've something important to say!' This sounded promising, certainly: Alice turned and came back again. 'Keep your temper,' said the Caterpillar. 'Is that all?' said Alice, swallowing down her anger as well as she  could. 'No,' said the Caterpillar. Alice thought she might as well wait, as she had nothing else to do, and  perhaps after all it might tell her something worth hearing.",
@@ -9,10 +9,11 @@ routerApp.controller('AboutController', function($scope) {
             copy: "'Come back!' the Caterpillar called after her. 'I've something important to say!' This sounded promising, certainly: Alice turned and came back again. 'Keep your temper,' said the Caterpillar. 'Is that all?' said Alice, swallowing down her anger as well as she  could. 'No,' said the Caterpillar. Alice thought she might as well wait, as she had nothing else to do, and  perhaps after all it might tell her something worth hearing.",
             image: "https://placeimg.com/640/480/any"
         }, {
-            copy: "'Come back!' the Caterpillar called after her. 'I've something important to say!' This sounded promising, certainly: Alice turned and came back again. 'Keep your temper,' said the Caterpillar. 'Is that all?' said Alice, swallowing down her anger as well as she  could. 'No,' said the Caterpillar. Alice thought she might as well wait, as she had nothing else to do, and  perhaps after all it might tell her something worth hearing.",
+            copy: "'Come back!' the Caterpillar called after her. 'I've something important to say!'",
             image: "https://placeimg.com/640/480/any"
         }
     ];
+
 
 });
 //Could inject data service her//Could inject data service herer});
