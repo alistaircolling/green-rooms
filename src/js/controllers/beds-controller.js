7422abd755d8d89994f9414dc052980c7c38c177
alistaircolling <alistair.colling@gmail.com>
2015-12-07 14:02:53 +0000

    adding price filter

--- src/js/controllers/beds-controller.js
+++ src/js/controllers/beds-controller.js
@@ -3,15 +3,19 @@ routerApp.controller('BedsController', function($scope) {
 
     $scope.message = 'test';
 
-    $scope.rooms = [{
+    $scope.rooms = [
+    {
         name: 'Basic',
-        price: 50
-    }, {
-        name: 'En-suite',
-        price: 1000
-    }, {
-        name: 'Presidential',
-        price: 50000000
-    }];
+        price: 50,
+        copy: 'A great option if you are on a budget or travelling in a large group.',
+        bullets: [{
+            copy: 'clean sheets'
+        }, {
+            copy: 'personal power supply'
+        }],
+        image:"http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg"
+    }
+    
+    ];
 
 });
