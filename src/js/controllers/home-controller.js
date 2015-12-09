479af79406df1773bd7bf1f4a2f2afb2712bfbe5
alistaircolling <alistair.colling@gmail.com>
2015-12-09 14:13:48 +0000

    ..

--- src/js/controllers/home-controller.js
+++ src/js/controllers/home-controller.js
@@ -1,6 +1,5 @@
 routerApp.controller('home-controller', function($scope) {
-
     $scope.phoneNumberDisplay = '+44(0)207800880';
     $scope.phoneNumberDial = '+44207800880';
-    $scope.address = '100 KING ROAD<br/>LONDON<br/>N10 1RT';
+    $scope.address = '100 KING ROAD, WOOD GREEN, LONDON N10 1RT';
 });
