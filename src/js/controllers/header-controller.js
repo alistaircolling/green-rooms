fdf1a5282ba8b068942a650b020c40033eda1e10
alistaircolling <alistair.colling@gmail.com>
2016-04-06 08:17:41 +0100

    mobile header logic working

--- src/js/controllers/header-controller.js
+++ src/js/controllers/header-controller.js
@@ -8,22 +8,33 @@ routerApp.controller('HeaderController', function($scope, $rootScope, $window, D
         function(event, toState, toParams, fromState, fromParams) {
             console.log("STATE CHANGE:" + toState.name);
             if (DeviceService.isMobile) {
+            console.log("is mobile");
                 switch (toState.name) {
                     case "about":
-                        $scope.showHeader = function() {
+                        $scope.mainHeader = function() {
                             return "hide-header";
                         };
+                        $scope.mobileHeader = function() {
+                            return "show-header";
+                        };
                         break;
-                    case "beds":
-                        $scope.showHeader = function() {
+                    case "rooms":
+                    console.log("is beds or rooms");
+                        $scope.mainHeader = function() {
                             return "hide-header";
+                        };
+                        $scope.mobileHeader = function() {
+                            return "show-header";
                         };
                         break;
                     case "home-mobile":
-                        $scope.showHeader = function() {
+                        $scope.mainHeader = function() {
+                            return "hide-header";
+                        };
+                        $scope.mobileHeader = function() {
                             return "show-header";
                         };
-                        //Scroll to hide address bar on mobile
+                        //Scroll to show address bar on mobile
                         $window.addEventListener("load", function() {
                             // Set a timeout...
                             setTimeout(function() {
@@ -34,15 +45,59 @@ routerApp.controller('HeaderController', function($scope, $rootScope, $window, D
                         });
                         break;
                     default:
-                        $scope.showHeader = function() {
+                        $scope.mainHeader = function() {
+                            return "show-header";
+                        };
+                        $scope.mobileHeader = function() {
                             return "hide-header";
                         };
 
                 }
             } else {
-                $scope.showHeader = function() {
-                    return "hide-header";
-                };
+            console.log("not mobile");
+                switch (toState.name) {
+                    case "about":
+                        $scope.mainHeader = function() {
+                            return "hide-header";
+                        };
+                        $scope.mobileHeader = function() {
+                            return "hide-header";
+                        };
+                        break;
+                    case "rooms":
+                        $scope.mainHeader = function() {
+                            return "hide-header";
+                        };
+                        $scope.mobileHeader = function() {
+                            return "show-header";
+                        };
+                        break;
+                    case "home-mobile":
+                        $scope.mainHeader = function() {
+                            return "hide-header";
+                        };
+                        $scope.mobileHeader = function() {
+                            return "show-header";
+                        };
+                        //Scroll to show address bar on mobile
+                        $window.addEventListener("load", function() {
+                            // Set a timeout...
+                            setTimeout(function() {
+                                // Hide the address bar!
+                                console.log("HIOHIIII");
+                                $window.scrollTo(0, 1);
+                            }, 0);
+                        });
+                        break;
+                    default:
+                        $scope.mainHeader = function() {
+                            return "show-header";
+                        };
+                        $scope.mobileHeader = function() {
+                            return "hide-header";
+                        };
+
+                }
 
             }
         });
