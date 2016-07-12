a512c09c19b2cbca02447516a380eb0b9799d52e
alistaircolling <alistair.colling@gmail.com>
2016-07-12 13:44:48 +0100

    updated phone number

--- src/js/controllers/mobile-home-controller.js
+++ src/js/controllers/mobile-home-controller.js
@@ -1,5 +1,5 @@
 routerApp.controller('mobile-home-controller', function($scope, $sce) {
     $scope.phoneNumberDisplay = '+44(020) 8888 5317';
-    $scope.phoneNumberDial = '+442288885317';
+    $scope.phoneNumberDial = '+442088885317';
     $scope.address = '<span>13-27 STATION ROAD<br>WOOD GREEN<br>LONDON<br>N22 6UW</span>';
 });
