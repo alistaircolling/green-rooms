caf7c5e63b34d8bc9981176a35847fc8854fdcf1
alistaircolling <alistair.colling@gmail.com>
2016-08-01 15:33:45 +0100

    ..

--- src/js/controllers/mobile-home-controller.js
+++ src/js/controllers/mobile-home-controller.js
@@ -1,5 +1,6 @@
-routerApp.controller('mobile-home-controller', function($scope, $sce) {
+routerApp.controller('mobile-home-controller', function($scope, $state, $sce) {
     $scope.phoneNumberDisplay = '+44(020) 8888 5317';
     $scope.phoneNumberDial = '+442088885317';
     $scope.address = '<span>13-27 STATION ROAD<br>WOOD GREEN<br>LONDON<br>N22 6UW</span>';
+    $scope.state = $state;
 });
