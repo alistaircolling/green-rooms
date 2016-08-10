d1c39be7bde3076395593d60a60dcbfb0ec225c8
alistaircolling <alistair.colling@gmail.com>
2016-08-10 16:12:33 +0100

    fixed mobile dialing rather than going to booking site

--- src/js/controllers/mobile-home-controller.js
+++ src/js/controllers/mobile-home-controller.js
@@ -1,5 +1,6 @@
 routerApp.controller('mobile-home-controller', function($log, $scope, $state, $sce) {
     $scope.phoneNumberDisplay = '+44(020) 8888 5317';
+    $scope.bookingLink = 'https://live.high-level-software.com/hotels/greenrooms/availability';
     $scope.phoneNumberDial = '+442088885317';
     $scope.address = '<span>13-27 STATION ROAD<br>WOOD GREEN<br>LONDON<br>N22 6UW</span>';
     //$scope.$state = $state;
